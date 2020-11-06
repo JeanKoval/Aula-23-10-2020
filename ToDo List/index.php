904679423dec27d484ffc31d0e4b866fab6bd869
Jean Koval <69021077+JeanKoval@users.noreply.github.com>
2020-11-06 16:07:56 -0300

    Update index.php
    Atalizando php com retorno do select para filtrar o status da tarefa.

--- ToDo List/index.php
+++ ToDo List/index.php
@@ -7,6 +7,14 @@ require "src/classes/ArquivoTarefa.php";
 // esta linha redireciona qualquer requisição no index para o endereco
 // header('Location: /resource/lista_tarefas.html');
 
+$escolha = $_POST['slc'] ? $_POST['slc'] : 1;
+
+if(isset($_POST['slc'])){
+    $escolha = $_POST['slc'];
+}else{
+    $escolha = 1;
+}
+
 $template = file_get_contents('resource/lista_tarefas.html');
 
 $arquivoTarefa = new ArquivoTarefa('tarefas.json');
@@ -17,18 +25,57 @@ $modeloTarefa = file_get_contents('resource/tarefa.html');
 $linhasTabela = '';
 
 
-foreach ($listaTarefasJSON as $tarefa) {
-    $tr = '';
-    $tr = str_replace('#STATUS', $tarefa->legenda(), $modeloTarefa);
-    $tr = str_replace('#ID',     $tarefa->getId(), $tr);
-    $tr = str_replace('#NOME',  $tarefa->getNome(), $tr);
-    $tr = str_replace('#DATALIMITE', $tarefa->getDataLimite(), $tr);
-    $tr = str_replace('#MARCADO', $tarefa->getStatus() == 0 ? 'checked' : '', $tr);
-    $linhasTabela .= $tr;
+if ($escolha == 1){
+    foreach ($listaTarefasJSON as $tarefa) {
+        $tr = '';
+        $tr = str_replace('#STATUS', $tarefa->legenda(), $modeloTarefa);
+        $tr = str_replace('#ID',     $tarefa->getId(), $tr);
+        $tr = str_replace('#NOME',  $tarefa->getNome(), $tr);
+        $tr = str_replace('#DATALIMITE', $tarefa->getDataLimite(), $tr);
+        $tr = str_replace('#MARCADO', $tarefa->getStatus() == 0 ? 'checked' : '', $tr);
+        $tr = str_replace('#TODAS', $escolha == 1  ? 'selected' : '', $tr);
+        $linhasTabela .= $tr;
+    }
 }
+
+if ($escolha == 2){
+    foreach ($listaTarefasJSON as $tarefa) {
+        if ($tarefa->getStatus() == 0){
+            $tr = '';
+            $tr = str_replace('#STATUS', $tarefa->legenda(), $modeloTarefa);
+            $tr = str_replace('#ID',     $tarefa->getId(), $tr);
+            $tr = str_replace('#NOME',  $tarefa->getNome(), $tr);
+            $tr = str_replace('#DATALIMITE', $tarefa->getDataLimite(), $tr);
+            $tr = str_replace('#MARCADO', $tarefa->getStatus() == 0 ? 'checked' : '', $tr);
+            $tr = str_replace('#ENCERRADAS', $escolha == 2  ? 'selected' : '', $tr);
+            $linhasTabela .= $tr;
+        }
+    }
+}
+
+if ($escolha == 3){
+    if ($tarefa->getStatus() == 1){
+        $tr = '';
+        $tr = str_replace('#STATUS', $tarefa->legenda(), $modeloTarefa);
+        $tr = str_replace('#ID',     $tarefa->getId(), $tr);
+        $tr = str_replace('#NOME',  $tarefa->getNome(), $tr);
+        $tr = str_replace('#DATALIMITE', $tarefa->getDataLimite(), $tr);
+        $tr = str_replace('#MARCADO', $tarefa->getStatus() == 0 ? 'checked' : '', $tr);
+        $tr = str_replace('#ABERTAS', $escolha == 3  ? 'selected' : '', $tr);
+        $linhasTabela .= $tr;
+    }
+}
+
+// foreach ($listaTarefasJSON as $tarefa) {
+//     $tr = '';
+//     $tr = str_replace('#STATUS', $tarefa->legenda(), $modeloTarefa);
+//     $tr = str_replace('#ID',     $tarefa->getId(), $tr);
+//     $tr = str_replace('#NOME',  $tarefa->getNome(), $tr);
+//     $tr = str_replace('#DATALIMITE', $tarefa->getDataLimite(), $tr);
+//     $tr = str_replace('#MARCADO', $tarefa->getStatus() == 0 ? 'checked' : '', $tr);
+//     $linhasTabela .= $tr;
+// }
 
 echo str_replace('#TAREFAS', $linhasTabela, $template);
 
-$escolha = $_POST['slc'];
 
-echo "a escolha foi $escolha";
